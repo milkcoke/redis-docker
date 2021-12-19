033619f1196d907e57c671401dd694d46b6bf5a6
milkcoke <mbh023@konkuk.ac.kr>
2021-12-20 04:00:35 +0900

    test: Test multiple redis client connection which has different port

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-import redis, { createClient } from 'redis';
+import redis, {createClient} from 'redis';
 
-(async () => {
-    const client = redis.createClient();
+const PORTS = [6379, 7000, 7100, 7200, 7300, 7400, 7500];
+
+
+async function main1() {
+    const client = createClient({
+        // url: 'redis://localhost:' + PORTS[0],
+        url: 'redis://localhost:' + 8000,
+    });
 
     client.on('error', (err : unknown) => {
         if (err instanceof Error) {
@@ -12,11 +18,123 @@ import redis, { createClient } from 'redis';
 
     await client.connect();
 
-    await client.set('key', 'value');
-    await client.set('name', 'jayce');
-    await client.set('age', 15);
-    await client.set('obj', Buffer.from(JSON.stringify({its: 'ld'})));
-    const value = await client.get('key');
-    console.dir(value);
+    // await client.set('PORT', PORTS[0]);
+    await client.set('PORT', 8000);
 
-})();
+    // await client.set('obj', Buffer.from(JSON.stringify({its: 'ld'})));
+
+    const port = await client.get('PORT');
+    console.log(port);
+
+}
+
+async function main2() {
+    const client = createClient({
+        url: 'redis://localhost:' + PORTS[2],
+    });
+
+    client.on('error', (err : unknown) => {
+        if (err instanceof Error) {
+            console.error(err);
+        }
+        console.log('Redis Client Error', err)
+    });
+
+    await client.connect();
+
+    await client.set('port', PORTS[2]);
+    const port = await client.get('port');
+
+    console.log(port)
+
+}
+
+async function main3() {
+    const client = createClient({
+        url: 'redis://localhost:' + PORTS[3],
+    });
+
+    client.on('error', (err : unknown) => {
+        if (err instanceof Error) {
+            console.error(err);
+        }
+        console.log('Redis Client Error', err)
+    });
+
+    await client.connect();
+
+    await client.set('port', PORTS[3]);
+    const port = await client.get('port');
+
+    console.log(port);
+
+}
+
+async function main4() {
+    const client = createClient({
+        url: 'redis://localhost:' + PORTS[4]
+    });
+
+    client.on('error', (err : unknown) => {
+        if (err instanceof Error) {
+            console.error(err);
+        }
+        console.log('Redis Client Error', err)
+    });
+
+    await client.connect();
+
+    await client.set('port', PORTS[4]);
+    const port = await client.get('port');
+
+    console.log(port);
+
+}
+
+
+async function main5() {
+    const client = createClient({
+        url: 'redis://localhost:' + PORTS[5],
+    });
+
+    client.on('error', (err : unknown) => {
+        if (err instanceof Error) {
+            console.error(err);
+        }
+        console.log('Redis Client Error', err)
+    });
+
+    await client.connect();
+
+    await client.set('port', PORTS[5]);
+    const port = await client.get('port');
+
+    console.log(port);
+
+}
+
+async function main6() {
+    const client = createClient({
+        url: 'redis://localhost:' +  PORTS[6],
+    });
+
+    client.on('error', (err : unknown) => {
+        if (err instanceof Error) {
+            console.error(err);
+        }
+        console.log('Redis Client Error', err)
+    });
+
+    await client.connect();
+
+    await client.set('port',  PORTS[6]);
+    const port = await client.get('port');
+
+    console.log(port);
+
+}
+
+
+// main1();
+// main2().then(main3).then(main4)
+main1().then(main2);
